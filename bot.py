25366ad4f8dc686af523aca3c98f06de04634247
Doston Absalamov <sizning.email@example.com>
2025-11-16 22:45:52 +0800

    update bot

--- bot.py
+++ bot.py
@@ -383,7 +383,7 @@ class WithdrawState(StatesGroup):
     amount = State()
 
 MIN_WITHDRAW = 60000
-MAX_WITHDRAW = 100000
+MAX_WITHDRAW = 1100000
 
 @dp.message(F.text == "💰 Pul yechish")
 async def withdraw_cmd(message: types.Message, state: FSMContext):
